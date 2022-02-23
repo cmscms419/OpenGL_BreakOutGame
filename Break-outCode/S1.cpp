3eb4625e389449e9cd659d2d827a8a44da41da08
cmscms419 <72899627+cmscms419@users.noreply.github.com>
2022-02-23 11:21:07 +0900

    Break-out Bulid
    빌드를 해가지고 만들어 봤습니다.
    하지만 리소스를 빌드에 포함시키는
    것을 몰라서 빌드 후 텍스처 파일을 따로 불어왔습니다.

---
+++ Break-outCode/S1.cpp
@@ -0,0 +1,156 @@
+#define STB_IMAGE_IMPLEMENTATION 
+
+#include "model.h"
+#include <stb_image.h>
+
+Circle ball;
+Bar bar;
+Block block[MAX_Y][MAX_X];
+GLuint base;
+
+GLubyte* pbytes; // 데이터를 가리킬 포인터
+GLuint texture[5]; // 텍스처의 수
+
+int keys = 1;
+
+void Display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 전에 있는 원의 흔적을 지운다.
+	backGround(texture[3]);
+	
+	switch (keys)
+	{
+	case 1:
+		Title(texture[4]);
+		break;
+	case 2:
+		bar.init(texture[2]);
+		ball.init(texture[0]);
+		Block_init(block, MAX_X, MAX_Y, texture[1]);
+
+		Bound(bar, &ball);
+		Block_Bound(block, MAX_X, MAX_Y, &ball);
+		keys = ball.Circlemove();
+		break;
+	}
+
+	glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'r':
+		ball.x1 = bar.x + bar.lenght * 0.5;
+		ball.y1 = bar.y + bar.height + ball.rad;
+		Block_Reset(block);
+		keys = 2;
+	}
+}
+
+void Special(int key, int x, int y)
+{
+	switch (key) {
+	case GLUT_KEY_UP:
+		ball.dx += static_cast<GLfloat>(AOC);
+		ball.dy += static_cast<GLfloat>(AOC);
+		break;
+	case GLUT_KEY_DOWN:
+		ball.dx -= static_cast<GLfloat>(AOC);
+		ball.dy -= static_cast<GLfloat>(AOC);
+		break;
+	default:
+		bar.Barmove(key);
+		break;
+	}
+}
+
+void reshape_func(int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void MyTimer(int Value) {
+
+	// 화면을 다시 그렬줄 것을 명령한다.
+	glutPostRedisplay();
+
+	// 1번쨰 매개변수 : 얼마 후에 타이머를 실행 할 것인지
+	// 2번째 매개변수 : 타이머 이벤트를 발생하기 위해서 호출되어야 할 함수
+	// 3본째 매개변수 : 타이머 이벤트에게 넘겨주고 싶은 파라미터
+	glutTimerFunc(0.001, MyTimer, NULL);
+}
+
+void initTexture()
+{
+	glGenTextures(5, texture);
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("circle_color.bmp", &width, &height, &nrChannels, 0);
+
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	stbi_image_free(data);
+
+	data = stbi_load("block.BMP", &width, &height, &nrChannels, 0);
+
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	stbi_image_free(data);
+
+	data = stbi_load("bar_color.bmp", &width, &height, &nrChannels, 0);
+
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	stbi_image_free(data);
+
+	data = stbi_load("BackGround.png", &width, &height, &nrChannels, 0);
+
+	glBindTexture(GL_TEXTURE_2D, texture[3]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	stbi_image_free(data);
+
+	data = stbi_load("Title.bmp", &width, &height, &nrChannels, 0);
+
+	glBindTexture(GL_TEXTURE_2D, texture[4]);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	stbi_image_free(data);
+
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+}
+
+int main(int argc, char** argv)
+{
+
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(100, 100); // 실행창을 어디 위치에 보여줄지 표시해 준다.
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // 디스플레이 표면의 특징을 결정한다.
+	glutCreateWindow("OpenGL");
+
+	initTexture(); // Texture mapping
+
+	glutDisplayFunc(Display); // 그리기 전달함수 (인수는 그리기메서드)
+	glutReshapeFunc(reshape_func); // 윈도우 크기를 조절할 때, 사용할 함수를 지정한다.
+	glutKeyboardFunc(keyboard); // 키보드 입력
+	glutSpecialFunc(Special); // 키보드의 특수 키 입력
+	glutTimerFunc(0, MyTimer, NULL); // 선언된 이벤트를 무한으로 실행 시킨다.
+	glutMainLoop();
+
+	return 0;
+}
---
+++ Break-outCode/model.cpp
@@ -0,0 +1,399 @@
+#include "model.h"
+
+// Block
+static GLfloat blc_x = BLOCK_INI_X;
+static GLfloat blc_y = BLOCK_INI_Y;
+
+static GLfloat blc_l = 0.2f;
+static GLfloat blc_h = 0.1f;
+
+static GLfloat blcTex_x = 0.0f;
+static GLfloat blcTex_y = 0.0f;
+
+void Circle::init(GLuint texture)
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glTexCoord2f(0.5, 0.5);
+	glVertex2f(x1, y1);
+
+	for (angle = 1.0f; angle < 361.0f; angle += 0.2f)
+	{
+		this->x2 = this->x1 + cos(angle) * rad;
+		this->y2 = this->y1 + sin(angle) * rad;
+		glTexCoord2f(0.5, 0.5);
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+}
+
+int Circle::Circlemove()
+{
+	this->x1 += this->xDir * this->dx;
+	this->y1 += this->yDir * this->dy;
+
+	// 원이 화면 범위를 벗어나게 되면 -1를 곱해서 방향을 바꾼다.
+	// 원의 이동 범위는 -1 ~ 1이다.
+	if ((this->x1 - this->rad <= -0.999) || (this->x1 + this->rad >= 0.999))
+	{
+		xDir = -xDir;
+	}
+
+	if (this->y1 + this->rad >= 0.999)
+	{
+		yDir = -yDir;
+	}
+
+	if (this->y1 <= -0.999)
+	{
+		return 1;
+	}
+
+	return 2;
+}
+
+void Bar::init(GLuint texture)
+{
+	// bar의 위치를 계속 
+	this->collisionSquare = {
+		this->x - RADIUS,
+		this->x + this->lenght + RADIUS,
+		this->y + this->height + RADIUS,
+		this->y - RADIUS
+	};
+
+	glEnable(GL_TEXTURE_2D);
+	{
+		glBindTexture(GL_TEXTURE_2D, texture);
+		
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(x, y);
+		glTexCoord2f(0, 1);
+		glVertex2f(x, y + this->height);
+		glTexCoord2f(1, 1);
+		glVertex2f(x + this->lenght, y + this->height);
+		glTexCoord2f(1, 0);
+		glVertex2f(x + this->lenght, y);
+		glEnd();
+	}
+	glDisable(GL_TEXTURE_2D);
+}
+
+void Bar::Barmove(int key)
+{
+	switch (key)
+	{
+	case GLUT_KEY_RIGHT:
+		this->x += 0.035f;
+		break;
+	case GLUT_KEY_LEFT:
+		this->x -= 0.035f;
+		break;
+	}
+}
+
+void Block::init(GLfloat x1, GLfloat y1, GLuint texture)
+{
+	if (this->stay) 
+	{
+		this->x = x1;
+		this->y = y1;
+		this->height = BLOCK_height;
+		this->lenght = BLOCK_lenght;
+
+		this->collisionSquare =
+		{
+			this->x - RADIUS,
+			this->x + this->lenght + RADIUS,
+			this->y + this->height + RADIUS,
+			this->y - RADIUS
+		};
+		
+		glEnable(GL_TEXTURE_2D);
+		{
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glBegin(GL_QUADS);
+			
+			glTexCoord2f(blcTex_x, blcTex_y + 0.25f);
+			glVertex2f(x, y);
+			
+			glTexCoord2f(blcTex_x + 0.06f, blcTex_y + 0.25f);
+			glVertex2f(x + this->lenght, y);
+
+			glTexCoord2f(blcTex_x + 0.06f, blcTex_y);
+			glVertex2f(x + this->lenght, y + this->height);
+			
+			glTexCoord2f(blcTex_x, blcTex_y);
+			glVertex2f(x, y + this->height);
+		
+			glEnd();
+		}
+		glDisable(GL_TEXTURE_2D);
+	}
+}
+
+int checkpointInCircle(Circle c, GLfloat x, GLfloat y)
+{
+	float dx = c.x1 - x;
+	float dy = c.y1 - y;
+	float lenght = sqrt(dx * dx + dy * dy);
+
+	if (lenght > c.rad)
+		return 0;
+	return 1;
+}
+
+int collisionSquareCircle(Bar bar, Circle circle)
+{
+
+	if ((bar.collisionSquare.Left < circle.x1 && circle.x1 < bar.collisionSquare.Right) &&
+		bar.collisionSquare.Bottom < circle.y1 && circle.y1 < bar.collisionSquare.Top)
+	{
+		// 왼쪽
+		if (bar.x > circle.x1 && bar.y < circle.y1 && circle.y1 < bar.y + bar.height)
+			return 1;
+		// 오른쪽
+		if (bar.x + bar.lenght < circle.x1 && bar.y < circle.y1 && circle.y1 < bar.y + bar.height)
+			return 1;
+		// 아래
+		if (bar.y > circle.y1 && bar.x < circle.x1 && circle.x1 < bar.x + bar.lenght)
+			return 2;
+		// 위
+		if (bar.y + bar.height < circle.y1 && bar.x < circle.x1 && circle.x1 < bar.x + bar.lenght)
+			return 2;
+
+		// 좌상단, 좌하단, 우상단, 우하단에 있는 것을 판단한다.
+		// 좌상단
+		if (checkpointInCircle(circle, bar.x, bar.y + bar.height))
+			return 3;
+		// 좌하단
+		if (checkpointInCircle(circle, bar.x, bar.y))
+			return 3;
+		// 우상단
+		if (checkpointInCircle(circle, bar.x + bar.lenght, bar.y + bar.height))
+			return 3;
+		// 우하단
+		if (checkpointInCircle(circle, bar.x + bar.lenght, bar.y))
+			return 3;
+
+		if (bar.x <= circle.x1 && circle.x1 <= bar.x + bar.lenght && bar.y <= circle.y1 && circle.y1 <= bar.y + bar.height)
+			return 4;
+	}
+	
+	return 0;
+}
+
+int collisionSquareCircle2(Block bar, Circle circle)
+{
+	// 위, 아래, 왼쪽, 오른쪽에 있는지 판단한다.
+	if ((bar.collisionSquare.Left < circle.x1 && circle.x1 < bar.collisionSquare.Right) &&
+		bar.collisionSquare.Bottom < circle.y1 && circle.y1 < bar.collisionSquare.Top)
+	{
+		// 왼쪽
+		if (bar.x > circle.x1 && bar.y < circle.y1 && circle.y1 < bar.y + bar.height)
+			return 1;
+		// 오른쪽
+		if (bar.x + bar.lenght < circle.x1 && bar.y < circle.y1 && circle.y1 < bar.y + bar.height)
+			return 1;
+		// 아래
+		if (bar.y > circle.y1 && bar.x < circle.x1 && circle.x1 < bar.x + bar.lenght)
+			return 2;
+		// 위
+		if (bar.y + bar.height < circle.y1 && bar.x < circle.x1 && circle.x1 < bar.x + bar.lenght)
+			return 2;
+		// 좌상단
+		if (checkpointInCircle(circle, bar.x, bar.y + bar.height))
+			return 3;
+		// 좌하단
+		if (checkpointInCircle(circle, bar.x, bar.y))
+			return 3;
+		// 우상단
+		if (checkpointInCircle(circle, bar.x + bar.lenght, bar.y + bar.height))
+			return 3;
+		// 우하단
+		if (checkpointInCircle(circle, bar.x + bar.lenght, bar.y))
+			return 3;
+		if (bar.x <= circle.x1 && circle.x1 <= bar.x + bar.lenght && bar.y <= circle.y1 && circle.y1 <= bar.y + bar.height)
+			return 4;
+	}
+	return 0;
+}
+
+void Bound(Bar bar, Circle* circle)
+{
+	GLfloat xvecter = circle->x1 - bar.x - (bar.lenght * 0.5f);
+	GLfloat yvecter = circle->y1 - bar.y - (bar.height * 0.5f);
+	GLfloat nor = sqrt(xvecter * xvecter + yvecter * yvecter);
+
+	switch (collisionSquareCircle(bar, *circle))
+	{
+	case 1:
+		circle->xDir *= -1.0f;
+		break;
+	case 2:
+		xvecter = xvecter / nor;
+		yvecter = yvecter / nor;
+
+		circle->xDir = xvecter;
+		circle->yDir *= -1.0f;
+
+		if (circle->yDir <= 0)
+			circle->yDir -= AOC;
+		else
+			circle->yDir += AOC;
+
+		printf("circle->xDir = %f\n", xvecter);
+		printf("circle->yDir = %f\n", yvecter);
+		break;
+	case 3:
+		xvecter = xvecter / nor;
+		yvecter = yvecter / nor;
+		
+		circle->xDir = xvecter;
+		circle->yDir *= -1.0f;
+
+		if (circle->yDir <= 0)
+			circle->yDir -= AOC;
+		else
+			circle->yDir += AOC;
+
+		printf("circle->xDir = %f\n", xvecter);
+		printf("circle->yDir = %f\n\n", yvecter);
+		break;
+
+	case 4:
+		circle->y1 += bar.height;
+		break;
+	}
+}
+
+void Bound2(Block* block, Circle* circle)
+{
+	switch (collisionSquareCircle2(*block, *circle))
+	{
+	case 1:
+		circle->xDir = -circle->xDir;
+		Del(block);
+		break;
+	case 2:
+		circle->yDir = -circle->yDir;
+		Del(block);
+		break;
+	case 3:
+		circle->xDir = -circle->xDir;
+		circle->yDir = -circle->yDir;
+		Del(block);
+		break;
+	case 4:
+		break;
+	}
+}
+
+void Block_init(Block block[][MAX_X], int max_x, int max_y, GLuint texture)
+{
+	for (int i = 0; i < max_y; i++)
+	{
+		for (int m = 0; m < max_x; m++)
+		{
+			block[i][m].init(blc_x, blc_y, texture);
+			blc_x += 0.12f;
+			blcTex_x += 0.06f;
+		}
+		blc_x = BLOCK_INI_X;
+		blc_y -= 0.1f;
+
+		blcTex_x = 0;
+		blcTex_y += 0.25f;
+	}
+	blc_y = BLOCK_INI_Y;
+	blcTex_y = 0.0f;
+}
+
+void Block_Bound(Block block[][MAX_X], int max_x, int max_y, Circle* ball)
+{
+	for (int i = 0; i < MAX_Y; i++)
+	{
+		for (int m = 0; m < MAX_X; m++)
+		{
+			if(&block[i][m])
+				Bound2(&block[i][m], ball);
+		}
+		blc_x = BLOCK_INI_X;
+		blc_y += 0.1f;
+	}
+	blc_y = BLOCK_INI_Y;
+}
+
+void Block_Reset(Block block[][MAX_X])
+{
+	for (int i = 0; i < MAX_Y; i++)
+	{
+		for (int m = 0; m < MAX_X; m++)
+		{
+			block[i][m].stay++;
+		}
+	}
+}
+
+void Del(Block* block)
+{
+	block->stay = NULL;
+	block->x = _NULL_LOC;
+	block->y = _NULL_LOC;
+	block->height = NULL;
+	block->lenght = NULL;
+	block->collisionSquare = { NULL,NULL,NULL,NULL };
+}
+
+void backGround(GLuint texture)
+{
+	glEnable(GL_TEXTURE_2D);
+	{
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(-1, 1);
+
+		glTexCoord2f(1, 0);
+		glVertex2f(1, 1);
+		
+		glTexCoord2f(1, 1);
+		glVertex2f(1, -1);
+
+		glTexCoord2f(0, 1);
+		glVertex2f(-1, -1);
+		glEnd();
+	}
+	glDisable(GL_TEXTURE_2D);
+}
+
+void Title(GLuint texture)
+{
+	glEnable(GL_TEXTURE_2D);
+	{
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(-0.5, 0.5);
+
+		glTexCoord2f(1, 0);
+		glVertex2f(0.5, 0.5);
+
+		glTexCoord2f(1, 1);
+		glVertex2f(0.5, 0.2);
+
+		glTexCoord2f(0, 1);
+		glVertex2f(-0.5, 0.2);
+		glEnd();
+	}
+	glDisable(GL_TEXTURE_2D);
+}
+
+void menu()
+{
+
+}
